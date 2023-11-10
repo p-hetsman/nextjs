0493196d417214587b37487db8138f110a95592b
Pavel Hetsman <kv4sha@gmail.com>
2023-11-10 11:55:10 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import { inter } from '@/app/ui/fonts';
+
+import '@/app/ui/global.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
